b03683052acf15f10a34355deb0ef92506f6c717
Ankit Muchhala <muchhalaankit@gmail.com>
2016-10-24 20:04:13 +0530

    directed and weighted edgeses

--- csvreader.py
+++ csvreader.py
@@ -13,13 +13,19 @@ countries = open('data/countries.json', 'r')
 def get_year_data(year, maxval):
     year = str(year)
     csvfile.seek(0)
-    csvfile.readline()
+    csvfile.readline()         #skip the CSV header
     codes = json.load(countries)
     reader = csv.reader(csvfile, delimiter=',')
     edgelist = []
+    edgeweights = []
     for row in reader:
         if row[2] == year:
-            if row[3] not in codes or row[4] not in codes or float(row[5]) < maxval or float(row[6]) < maxval:
+            if row[3] not in codes or row[4] not in codes:
                 continue
-            edgelist.append((int(codes[row[3]]), int(codes[row[4]])))
-    return edgelist
+            if float(row[5]) >= maxval:
+                edgelist.append((int(codes[row[3]]), int(codes[row[4]])))
+                edgeweights.append(float(row[5]))
+            if float(row[6]) >= maxval:
+                edgelist.append((int(codes[row[4]]), int(codes[row[3]])))
+                edgeweights.append(float(row[6]))
+    return (edgelist, edgeweights)
--- main.py
+++ main.py
@@ -4,19 +4,24 @@ import csvreader
 import helpers
 
 vertices = helpers.get_countries()
-edges = csvreader.get_year_data(2009, 500.0)
+edges, weights = csvreader.get_year_data(2009, 10000.0)
 
-g = Graph(vertex_attrs={"label": vertices}, edges=edges)
+g = Graph(vertex_attrs={"label": vertices}, edges=edges, directed=True)
 g.vs["name"] = vertices
+g.es["weight"] = weights
 
 to_delete_ids = [v.index for v in g.vs if g.degree(v) == 0]
 g.delete_vertices(to_delete_ids)
 
 # print g.degree_distribution(bin_width = 1)
-# print "Network Diameter: %d" % g.diameter()
-# print "Largest Clique Size: %d" % g.omega()
+print "Network Diameter: %d" % g.diameter()
+print "Largest Clique Size: %d" % g.omega()
 # plt.plot(g.closeness(vertices=None, mode=ALL, cutoff=None, weights=None, normalized=True))
 # plt.show()
 
-# layout = g.layout("kk")
-# plot(g, layout = layout)
+print g.degree()
+layout = g.layout("fr")
+plot(g, layout = layout)
+
+# plt.hist(g.degree())
+# plt.show()
