2583aced25fc78da8b7abe6c5a35ac244d1943e7
Ankit Muchhala <muchhalaankit@gmail.com>
2016-10-25 13:24:21 +0530

    graph metrics

--- main.py
+++ main.py
@@ -16,10 +16,14 @@ g.delete_vertices(to_delete_ids)
 # print g.degree_distribution(bin_width = 1)
 print "Network Diameter: %d" % g.diameter()
 print "Largest Clique Size: %d" % g.omega()
+# print "Clustering Coefficient: %d" % g
+print "Reciprocity: %d" % g.reciprocity(mode="ratio")
+print "Mean Degree: %d" % mean(g.degree())
+
+# g.write_gml('test.gml')
 # plt.plot(g.closeness(vertices=None, mode=ALL, cutoff=None, weights=None, normalized=True))
 # plt.show()
 
-print g.degree()
 layout = g.layout("fr")
 plot(g, layout = layout)
 
